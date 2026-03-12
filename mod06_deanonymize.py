600d0f09ed0d637cd3f4f8d552b1972f9ea02199
hiitsme <dingdong69@gmail.com>
2026-03-11 23:27:09 -0400

    attempted homework 6

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -18,7 +18,22 @@ def link_records(anon_df, aux_df):
       anon_id, matched_name
     containing ONLY uniquely matched records.
     """
-    raise NotImplementedError
+    match_dict = {"anon_id":[], "matched_id":[]}
+    anon_dict = anon_df.to_dict()
+    aux_dict = aux_df.to_dict()
+    for i in reac
+    potential_matches = []
+    pm_scores = []
+    for i in range(len(aux_df)):
+        potential_matches.append(aux_dict["name"][i])
+
+    
+    for pm in potential_matches:
+        pm_score = 0;
+        
+        
+
+    #raise NotImplementedError
 
 
 def deanonymization_rate(matches_df, anon_df):
